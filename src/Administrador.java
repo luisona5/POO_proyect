8c5e9e0d81dbefaf8dfdef7d42b203010ca4f874
luisona5 <150484099+luisona5@users.noreply.github.com>
2025-02-06 16:24:28 -0500

    registro de usuarios al sistema

--- src/Administrador.java
+++ src/Administrador.java
@@ -13,28 +13,43 @@ public class Administrador {
     private JTextField textField3;
     private JComboBox comboBox1;
     private JButton siguienteButton;
+    private JTextField textField4;
+    private JTextField textField6;
 
     public Administrador() {
         siguienteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                    String DNI=textField2.getText();
+                    String nombre=textField1.getText();
+                    String telefono=textField3.getText();
+                    String correo=textField6.getText();
+                    String direccion=textField4.getText();
+                    String Usuarioregistro=comboBox1.getSelectedItem().toString();
+
                 try{
                     MongoDatabase database = ConexionMongoDB.getDatabase(); // Obtener la base de datos
                     MongoCollection<Document> collection = database.getCollection("usuarios");
-                        /*
+
                     // Crear el nuevo documento
-                    Document nuevo = new Document("usuario", nombre)
-                            .append("ingreso", Items)
-                            .append("contraseña", Password);
+                    Document nuevo  = new Document("usuario",nombre)
+                            .append("cedula",DNI)
+                            .append("ingreso", Usuarioregistro)
+                            .append("correo",correo)
+                            .append("direccion",direccion)
+                            .append("telefono",telefono);
 
                     // Insertar el documento en la colección
                     collection.insertOne(nuevo);
+                    if (nuevo != null) {
+                        JOptionPane.showMessageDialog(null, "Usuario registrado correctamente.\n puede procedecer a realizar la reserva");
 
-                    JOptionPane.showMessageDialog(null, "Usuario registrado correctamente.");
-                    // ... (limpiar los campos del formulario, etc.)
+                    }else {
+                        JOptionPane.showMessageDialog(null, "Verificar datos");
+                    }
 
 
-                        */
 
                 } catch (Exception ex) {
                     throw new RuntimeException(ex);
